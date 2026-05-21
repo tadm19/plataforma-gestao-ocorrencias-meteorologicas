44b3e53c1cf0a5431d4234da6a6c649cd7584953
Afonso Mendonça <afonsomendonca11@gmail.com>
2026-05-21 01:32:25 +0100

    Corrigir rotas do frontend

--- frontend/server.js
+++ frontend/server.js
@@ -5,39 +5,30 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const rootDir = __dirname;
 
-app.get("/", (req, res) => {
-  res.redirect(302, "/index.html");
+const pages = {
+  "/": "index.html",
+  "/index": "index.html",
+  "/lista": "lista.html",
+  "/nova": "nova.html",
+  "/detalhe": "detalhe.html",
+  "/editar": "editar.html",
+  "/login": "login.html",
+  "/registo": "registo.html",
+};
+
+Object.entries(pages).forEach(([route, file]) => {
+  app.get(route, (req, res) => {
+    res.sendFile(path.join(rootDir, file));
+  });
 });
 
-app.get("/lista", (req, res) => {
-  res.sendFile(path.join(rootDir, "lista.html"));
-});
-
-app.get("/nova", (req, res) => {
-  res.sendFile(path.join(rootDir, "nova.html"));
-});
-
-app.get("/detalhe", (req, res) => {
-  res.sendFile(path.join(rootDir, "detalhe.html"));
-});
-
-app.get("/editar", (req, res) => {
-  res.sendFile(path.join(rootDir, "editar.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(rootDir, "login.html"));
-});
-
-app.get("/registo", (req, res) => {
-  res.sendFile(path.join(rootDir, "registo.html"));
-});
-
-app.use(express.static(rootDir, {
-  extensions: false,
-  index: false,
-  redirect: false,
-}));
+app.use(
+  express.static(rootDir, {
+    extensions: false,
+    index: "index.html",
+    redirect: false,
+  })
+);
 
 app.use((req, res) => {
   res.status(404).send("Not found");
