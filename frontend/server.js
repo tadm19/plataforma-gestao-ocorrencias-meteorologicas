b2cb0ce79c6a432eea9954af0cd6909d95c271ed
Afonso Mendonça <afonsomendonca11@gmail.com>
2026-05-21 01:25:39 +0100

    Suportar rotas frontend no App Service

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,13 +9,35 @@ app.get("/", (req, res) => {
   res.redirect(302, "/index.html");
 });
 
-app.use(
-  express.static(rootDir, {
-    extensions: false,
-    index: false,
-    redirect: false,
-  })
-);
+app.get("/lista", (req, res) => {
+  res.sendFile(path.join(rootDir, "lista.html"));
+});
+
+app.get("/nova", (req, res) => {
+  res.sendFile(path.join(rootDir, "nova.html"));
+});
+
+app.get("/detalhe", (req, res) => {
+  res.sendFile(path.join(rootDir, "detalhe.html"));
+});
+
+app.get("/editar", (req, res) => {
+  res.sendFile(path.join(rootDir, "editar.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(rootDir, "login.html"));
+});
+
+app.get("/registo", (req, res) => {
+  res.sendFile(path.join(rootDir, "registo.html"));
+});
+
+app.use(express.static(rootDir, {
+  extensions: false,
+  index: false,
+  redirect: false,
+}));
 
 app.use((req, res) => {
   res.status(404).send("Not found");
